edc49f976aa974e3723cb067287ad8e9e2726723
Alberto NM <alberto-city@hotmail.com>
2025-07-14 00:34:38 -0600

    feat: add authentication with JWT and bcrypt for login and user creation

--- src/db.js
+++ src/db.js
@@ -3,4 +3,5 @@ import "dotenv/config";
 
 export const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: "postgres",
+  logging: false
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { Op } from "sequelize";
 import { z } from "zod";
 import { User } from "./schema.js";
@@ -9,13 +11,66 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello Chichard");
+  res.send("Hello world!");
+});
+
+const loginSchema = z.object({
+  mail: z.email(),
+  password: z.string(),
+});
+
+app.post("/login", async (req, res) => {
+  const body = loginSchema.safeParse(req.body);
+
+  if (!body.success) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const [user] = await User.findAll({
+    where: {
+      mail: body.data.mail,
+    },
+  });
+
+  if (!user) {
+    res.sendStatus(404);
+    return;
+  }
+
+  const passwordMatch = await bcrypt.compare(body.data.password, user.password);
+
+  if (!passwordMatch) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const token = jwt.sign({...user}, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+  res.json({ token });
+});
+
+app.use((req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    res.sendStatus(403);
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(authHeader.substring(7), process.env.JWT_SECRET);
+    next()
+  } catch {
+    res.sendStatus(401);
+    return;
+  }
+
 });
 
 app.get("/users", async (req, res) => {
-  
   const filters = [];
-  
+
   if (req.query.name) {
     filters.push({ name: { [Op.iLike]: `%${req.query.name}%` } });
   }
@@ -47,7 +102,9 @@ app.post("/users", async (req, res) => {
     return;
   }
 
-  const user = await User.create(body.data);
+  const hashedPassword = await bcrypt.hash(body.data.password, 10);
+
+  const user = await User.create({ ...body.data, password: hashedPassword });
   res.status(201);
   res.json(user);
 });
