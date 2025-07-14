2fc3c6f3d5c5285d732c49a129045f038574d916
Alberto NM <alberto-city@hotmail.com>
2025-07-13 18:09:33 -0600

    feat: initialize project with Express and Sequelize setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello Chichard");
+});
+
+app.listen(3000, ()=>{
+    console.log("app listening on port 3000");
+});
